91765e7637113aab08470d565f0f7b2216ea0f80
James Carlson <jxxcarlson@gmail.com>
2021-12-08 23:25:05 -0500

    Add README

--- src/L0.elm
+++ src/L0.elm
@@ -1,4 +1,18 @@
-module L0 exposing (AST, parse)
+module L0 exposing
+    ( AST
+    , parse
+    )
+
+{-| A Parser for the experimental L0 module. See the app folder to see how it is used.
+The Render folder in app could have been included with the parser. However, this way
+users are free to design their own renderer.
+
+Since this package is still experimental (but needed in various test projects).
+The documentation is skimpy.
+
+@docs AST, parser
+
+-}
 
 import Block.Block
 import Tree exposing (Tree)
@@ -6,6 +20,7 @@ import Tree.BlocksV
 import Tree.Build exposing (Error)
 
 
+{-| -}
 type alias AST =
     List (Tree Block.Block.L0BlockE)
 
@@ -15,6 +30,7 @@ isVerbatimLine str =
     String.left 2 str == "||"
 
 
+{-| -}
 parse : String -> AST
 parse sourceText =
     sourceText
